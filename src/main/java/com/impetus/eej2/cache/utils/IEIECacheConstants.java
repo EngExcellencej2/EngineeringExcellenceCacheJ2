e2b71740bf7cd937f187a1e0f0eeb1e0c04a526b
Hitesh Pawar <hitesh.engineer85@gmail.com>
2014-07-18 18:40:20 +0530

    TK-01144:Check-in for Connection Pooling Change

---
+++ src/main/java/com/impetus/eej2/cache/utils/IEIECacheConstants.java
@@ -0,0 +1,17 @@
+package com.impetus.eej2.cache.utils;
+
+public interface IEIECacheConstants {
+
+	public static final String LOCAL_DC_NAME = "LOCAL_DC_NAME";
+	
+	public static final String NODES = "NODES";
+	
+	public static final String CORE_CONN_PER_HOST = "CORE_CONN_PER_HOST";
+	
+	public static final String MAX_CONN_PER_HOST = "MAX_CONN_PER_HOST";
+	
+	public static final String MAX_SIMULTANEOUS_REQ_PER_CONN_THRESHHOLD = "MAX_SIMULTANEOUS_REQ_PER_CONN_THRESHHOLD";
+	
+	public static final String MIN_SIMULTANEOUS_REQ_PER_CONN_THRESHHOLD = "MIN_SIMULTANEOUS_REQ_PER_CONN_THRESHHOLD";
+	
+}
