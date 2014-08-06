146be34d94ba5a5c26031594f35541d978d8c2ce
Hitesh <hitesh.engineer85@gmail.com>
2014-08-06 11:53:28 +0530

    Correct Java Doc

--- src/main/java/com/impetus/eej2/cache/entity/EIERequest.java
+++ src/main/java/com/impetus/eej2/cache/entity/EIERequest.java
@@ -1,29 +1,30 @@
 package com.impetus.eej2.cache.entity;
 
 /**
+ * <p>
+ * An entry to represent input request to EIE Cache utils
+ * </p>
+ * 
  * @author hitesh.pawar
- *         <p>
- *         An entry to represent input request to EIE Cache utils
- *         </p>
  * @version 0.1
  */
 public class EIERequest {
-	
+
 	/**
 	 * <p>
 	 * Returns <code>EIERequest</code> object attributes.
 	 * </p>
 	 */
-	public String toString(){
+	public String toString() {
 		StringBuilder sb = new StringBuilder("EIE Request [");
-		sb.append("TN:"+TN+"CC:"+CC+"]");
+		sb.append("TN:" + TN + "CC:" + CC + "]");
 		return sb.toString();
 	}
 
 	private String TN;
 
 	private String CC;
-	
+
 	private int TTL;
 
 	public int getTTL() {
