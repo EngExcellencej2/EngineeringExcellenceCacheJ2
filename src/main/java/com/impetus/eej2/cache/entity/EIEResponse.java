86c28266165bf5b023f17fb6b39396de01382831
Madan <devendramadaan2007@gmail.com>
2014-09-03 14:38:22 +0530

    Implemented Serializable interface

--- src/main/java/com/impetus/eej2/cache/entity/EIEResponse.java
+++ src/main/java/com/impetus/eej2/cache/entity/EIEResponse.java
@@ -1,6 +1,9 @@
 package com.impetus.eej2.cache.entity;
 
+import java.io.Serializable;
 import java.util.Date;
+
+import com.impetus.eej2.cache.utils.IEIECacheConstants;
 
 /**
  * \
@@ -11,54 +14,251 @@ import java.util.Date;
  *         </p>
  * @version 0.1
  */
-public class EIEResponse {
+public class EIEResponse implements Serializable {
+
+	private static final long serialVersionUID = 7966240255235119001L;
+	public static final String RESPONSE_KEY_VALUE_SEPARATOR=":";
 
 	private String id;
 
-	private String cc;
+	private String countryCode;
 
-	private String TN;
+	private String telephoneNumber;
 
-	private String MNC;
+	private String mnc;
 
-	private String MCC;
+	private String mcc;
 
-	private String SPID;
+	private String spId;
 
-	private Date crDate;
+	private Date createdDate;
 
-	private Long reqType;
+	private Long requestType;
 
-	private String resString;
+	private String responseString;
 
 	private Long status;
 
-	private Long IMSI;
+	private Long imsi;
 
-	private Long HLR;
+	private Long hlr;
 
-	private Long MSC;
+	private Long msc;
 
-	private String TN_Type;
+	private String tnType;
 
 	private String supplierType;
 
 	private Long supplierId;
+
+	private Integer timeToLive;
+
 	
-	private int TTL;
-
-	public int getTTL() {
-		return TTL;
-	}
-
-	public void setTTL(int tTL) {
-		TTL = tTL;
-	}
 
 	public String getId() {
 		return id;
 	}
-    
+
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+
+
+	public String getTelephoneNumber() {
+		return telephoneNumber;
+	}
+
+
+
+	public void setTelephoneNumber(String telephoneNumber) {
+		this.telephoneNumber = telephoneNumber;
+	}
+
+
+
+	public String getMnc() {
+		return mnc;
+	}
+
+
+
+	public void setMnc(String mnc) {
+		this.mnc = mnc;
+	}
+
+
+
+	public String getMcc() {
+		return mcc;
+	}
+
+
+
+	public void setMcc(String mcc) {
+		this.mcc = mcc;
+	}
+
+
+
+	public String getSpId() {
+		return spId;
+	}
+
+
+
+	public void setSpId(String spId) {
+		this.spId = spId;
+	}
+
+
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+
+
+	public Long getRequestType() {
+		return requestType;
+	}
+
+
+
+	public void setRequestType(Long requestType) {
+		this.requestType = requestType;
+	}
+
+
+
+	public String getResponseString() {
+		return responseString;
+	}
+
+
+
+	public void setResponseString(String responseString) {
+		this.responseString = responseString;
+	}
+
+
+
+	public Long getStatus() {
+		return status;
+	}
+
+
+
+	public void setStatus(Long status) {
+		this.status = status;
+	}
+
+
+
+	public Long getImsi() {
+		return imsi;
+	}
+
+
+
+	public void setImsi(Long imsi) {
+		this.imsi = imsi;
+	}
+
+
+
+	public Long getHlr() {
+		return hlr;
+	}
+
+
+
+	public void setHlr(Long hlr) {
+		this.hlr = hlr;
+	}
+
+
+
+	public Long getMsc() {
+		return msc;
+	}
+
+
+
+	public void setMsc(Long msc) {
+		this.msc = msc;
+	}
+
+
+
+	public String getTnType() {
+		return tnType;
+	}
+
+
+
+	public void setTnType(String tnType) {
+		this.tnType = tnType;
+	}
+
+
+
+	public String getSupplierType() {
+		return supplierType;
+	}
+
+
+
+	public void setSupplierType(String supplierType) {
+		this.supplierType = supplierType;
+	}
+
+
+
+	public Long getSupplierId() {
+		return supplierId;
+	}
+
+
+
+	public void setSupplierId(Long supplierId) {
+		this.supplierId = supplierId;
+	}
+
+
+
+	public Integer getTimeToLive() {
+		return timeToLive;
+	}
+
+
+
+	public void setTimeToLive(Integer timeToLive) {
+		this.timeToLive = timeToLive;
+	}
+
+
+
 	/**
 	 * <p>
 	 * Returns <code>EIEResponse</code> object attributes.
@@ -66,132 +266,23 @@ public class EIEResponse {
 	 */
 	public String toString(){
 		StringBuilder sb = new StringBuilder("EIE Response [");
-		sb.append("ID:"+id+"CC:"+cc+"TN:"+TN+"MNC:"+MNC+"MCC"+MCC+"SPID:"+SPID+"Created Date:"+crDate+"Request Type:"+reqType+"Response String:"+resString+
-				"Status:"+status+"IMSI:"+IMSI+"HLR:"+HLR+"MSC:"+MSC+"TN_TYPE:"+TN_Type+"Supplier Type:"+supplierType+"Supplier ID:"+supplierId+"]");
+		sb.append(IEIECacheConstants.ID).append(RESPONSE_KEY_VALUE_SEPARATOR).append(id)
+				.append(IEIECacheConstants.COUNTRY_CODE).append(RESPONSE_KEY_VALUE_SEPARATOR).append(countryCode)
+				.append(IEIECacheConstants.TELEPHONE_NUMBER).append(RESPONSE_KEY_VALUE_SEPARATOR).append(telephoneNumber)
+				.append(IEIECacheConstants.MNC).append(RESPONSE_KEY_VALUE_SEPARATOR).append(mnc)
+				.append(IEIECacheConstants.MCC).append(RESPONSE_KEY_VALUE_SEPARATOR).append(mcc)
+				.append(IEIECacheConstants.SPID).append(RESPONSE_KEY_VALUE_SEPARATOR).append(spId)
+				.append(IEIECacheConstants.CREATED_DATE).append(RESPONSE_KEY_VALUE_SEPARATOR).append(createdDate)
+				.append(IEIECacheConstants.REQUEST_TYPE).append(RESPONSE_KEY_VALUE_SEPARATOR).append(requestType)
+				.append(IEIECacheConstants.RESPONSE_STRING).append(RESPONSE_KEY_VALUE_SEPARATOR).append(responseString)
+				.append(IEIECacheConstants.STATUS).append(RESPONSE_KEY_VALUE_SEPARATOR).append(status)
+				.append(IEIECacheConstants.IMSI).append(RESPONSE_KEY_VALUE_SEPARATOR).append(imsi)
+				.append(IEIECacheConstants.HLR).append(RESPONSE_KEY_VALUE_SEPARATOR).append(hlr)
+				.append(IEIECacheConstants.MSC).append(RESPONSE_KEY_VALUE_SEPARATOR).append(msc)
+				.append(IEIECacheConstants.TN_TYPE).append(RESPONSE_KEY_VALUE_SEPARATOR).append(tnType)
+				.append(IEIECacheConstants.SUPPLIER_TYPE).append(RESPONSE_KEY_VALUE_SEPARATOR).append(supplierType)
+				.append(IEIECacheConstants.SUPPLIER_ID).append(RESPONSE_KEY_VALUE_SEPARATOR).append(supplierId).append("]");
 		return sb.toString();
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getCc() {
-		return cc;
-	}
-
-	public void setCc(String cc) {
-		this.cc = cc;
-	}
-
-	public String getTN() {
-		return TN;
-	}
-
-	public void setTN(String tN) {
-		TN = tN;
-	}
-
-	public String getMNC() {
-		return MNC;
-	}
-
-	public void setMNC(String mNC) {
-		MNC = mNC;
-	}
-
-	public String getMCC() {
-		return MCC;
-	}
-
-	public void setMCC(String mCC) {
-		MCC = mCC;
-	}
-
-	public String getSPID() {
-		return SPID;
-	}
-
-	public void setSPID(String sPID) {
-		SPID = sPID;
-	}
-
-	public Date getCrDate() {
-		return crDate;
-	}
-
-	public void setCrDate(Date crDate) {
-		this.crDate = crDate;
-	}
-
-	public Long getReqType() {
-		return reqType;
-	}
-
-	public void setReqType(Long reqType) {
-		this.reqType = reqType;
-	}
-
-	public String getResString() {
-		return resString;
-	}
-
-	public void setResString(String resString) {
-		this.resString = resString;
-	}
-
-	public Long getStatus() {
-		return status;
-	}
-
-	public void setStatus(Long status) {
-		this.status = status;
-	}
-
-	public Long getIMSI() {
-		return IMSI;
-	}
-
-	public void setIMSI(Long iMSI) {
-		IMSI = iMSI;
-	}
-
-	public Long getHLR() {
-		return HLR;
-	}
-
-	public void setHLR(Long hLR) {
-		HLR = hLR;
-	}
-
-	public Long getMSC() {
-		return MSC;
-	}
-
-	public void setMSC(Long mSC) {
-		MSC = mSC;
-	}
-
-	public String getTN_Type() {
-		return TN_Type;
-	}
-
-	public void setTN_Type(String tN_Type) {
-		TN_Type = tN_Type;
-	}
-
-	public String getSupplierType() {
-		return supplierType;
-	}
-
-	public void setSupplierType(String supplierType) {
-		this.supplierType = supplierType;
-	}
-
-	public Long getSupplierId() {
-		return supplierId;
-	}
-
-	public void setSupplierId(Long supplierId) {
-		this.supplierId = supplierId;
 	}
 
 }
