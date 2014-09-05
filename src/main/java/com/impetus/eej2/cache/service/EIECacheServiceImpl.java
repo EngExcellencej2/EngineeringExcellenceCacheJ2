6159675af789284cb8231ca8447e69fb6a5e4495
sharad <sharad.agarwal@impetus.co.in>
2014-09-05 12:04:51 +0530

    updated for exception handling

--- src/main/java/com/impetus/eej2/cache/service/EIECacheServiceImpl.java
+++ src/main/java/com/impetus/eej2/cache/service/EIECacheServiceImpl.java
@@ -47,72 +47,11 @@ public class EIECacheServiceImpl implements IEIECacheService {
 		ieieCacheDao = new EIECacheDaoImpl();
 	}
 
-	public EIEResponse getEIEresponse(EIERequest eieReq)
-			throws EieCacheCheckedException {
+	public EIEResponse getEIEresponse(EIERequest eieReq) throws EieCacheCheckedException
+			 {
 		logger.info("inside getEIEresponse of  EIECacheServiceImpl {}", eieReq);
 		EIEResponse eieResponse = null;
-		try {
-			eieResponse = ieieCacheDao.getEIEResponse(eieReq);
-		} catch (NoHostAvailableException noHostAvailableException) {
-			logger.error(
-					"no host in the cluster can be contacted successfully to execute this query ",
-					noHostAvailableException);
-			throw new EieCacheCheckedException(
-					EieCacheErrorCodes.UNSUCCESSFULL_READ,
-					"no host in the cluster can be contacted successfully to execute this query",
-					noHostAvailableException.toString());
-		} catch (QueryExecutionException queryExecutionException) {
-			logger.error(
-					"an exception thrown by Cassandra when it cannot execute the query with the requested consistency level successfully",
-					queryExecutionException);
-			throw new EieCacheCheckedException(
-					EieCacheErrorCodes.UNSUCCESSFULL_READ,
-					"an exception thrown by Cassandra when it cannot execute the query with the requested consistency level successfully",
-					queryExecutionException.toString());
-		} catch (QueryValidationException queryValidationException) {
-			logger.error(
-					" found syntax error, unauthorized or any other validation problem ",
-					queryValidationException);
-			throw new EieCacheCheckedException(
-					EieCacheErrorCodes.UNSUCCESSFULL_READ,
-					"found syntax error, unauthorized or any other validation problem",
-					queryValidationException.toString());
-		} catch (UnsupportedFeatureException unsupportedFeatureException) {
-			logger.error(
-					"BatchStatement, ResultSet paging and binary values in RegularStatement may be not supported ",
-					unsupportedFeatureException);
-			throw new EieCacheCheckedException(
-					EieCacheErrorCodes.UNSUCCESSFULL_READ,
-					"BatchStatement, ResultSet paging and binary values in RegularStatement may be not supported",
-					unsupportedFeatureException.toString());
-		} catch (IllegalArgumentException illegalArgumentException) {
-			logger.error(
-					"more values are provided than there is of bound variables in this statement. ",
-					illegalArgumentException);
-			throw new EieCacheCheckedException(
-					EieCacheErrorCodes.UNSUCCESSFULL_READ,
-					"more values are provided than there is of bound variables in this statement.",
-					illegalArgumentException.toString());
-		} catch (InvalidTypeException invalidTypeException) {
-			logger.error(
-					"any of the provided value is not of correct type to be bound to the corresponding bind variable ",
-					invalidTypeException);
-			throw new EieCacheCheckedException(
-					EieCacheErrorCodes.UNSUCCESSFULL_READ,
-					"any of the provided value is not of correct type to be bound to the corresponding bind variable",
-					invalidTypeException.toString());
-		} catch (NullPointerException nullPointerException) {
-			logger.error("null value found ", nullPointerException);
-			throw new EieCacheCheckedException(
-					EieCacheErrorCodes.UNSUCCESSFULL_READ, "null value found",
-					nullPointerException.toString());
-		} catch (Exception exception) {
-			logger.error("error during reading data ", exception);
-			throw new EieCacheCheckedException(
-					EieCacheErrorCodes.UNSUCCESSFULL_READ,
-					"error during reading data", exception.toString());
-		}
-
+				eieResponse = ieieCacheDao.getEIEResponse(eieReq);
 		return eieResponse;
 	}
 
@@ -121,71 +60,7 @@ public class EIECacheServiceImpl implements IEIECacheService {
 		logger.info("inside addEIEexternalResponse of  EIECacheServiceImpl {}",
 				eieRes);
 		Boolean writeFlag = false;
-		try
-		{
-			writeFlag = ieieCacheDao.addEIEExternalReponse(eieRes);
-		}
-		catch (NoHostAvailableException noHostAvailableException) {
-			logger.error("no host in the cluster can be contacted successfully to execute this query ",
-					noHostAvailableException);
-			throw new EieCacheCheckedException(
-					EieCacheErrorCodes.UNSUCCESSFULL_WRITE,
-					"no host in the cluster can be contacted successfully to execute this query",
-					noHostAvailableException.toString());
-		}
-		catch (QueryExecutionException queryExecutionException ) {
-			logger.error("an exception thrown by Cassandra when it cannot execute the query with the requested consistency level successfully",
-					queryExecutionException);
-			throw new EieCacheCheckedException(
-					EieCacheErrorCodes.UNSUCCESSFULL_WRITE,
-					"an exception thrown by Cassandra when it cannot execute the query with the requested consistency level successfully",
-					queryExecutionException.toString());
-		}
-		catch (QueryValidationException queryValidationException) {
-			logger.error(" found syntax error, unauthorized or any other validation problem ",
-					queryValidationException);
-			throw new EieCacheCheckedException(
-					EieCacheErrorCodes.UNSUCCESSFULL_WRITE,
-					"found syntax error, unauthorized or any other validation problem",
-					queryValidationException.toString());
-		}
-		catch (UnsupportedFeatureException unsupportedFeatureException) {
-			logger.error("BatchStatement, ResultSet paging and binary values in RegularStatement may be not supported ",
-					unsupportedFeatureException);
-			throw new EieCacheCheckedException(
-					EieCacheErrorCodes.UNSUCCESSFULL_WRITE,
-					"BatchStatement, ResultSet paging and binary values in RegularStatement may be not supported",
-					unsupportedFeatureException.toString());
-		}
-		catch (IllegalArgumentException illegalArgumentException) {
-			logger.error("more values are provided than there is of bound variables in this statement. ",
-					illegalArgumentException);
-			throw new EieCacheCheckedException(
-					EieCacheErrorCodes.UNSUCCESSFULL_WRITE,
-					"more values are provided than there is of bound variables in this statement.",
-					illegalArgumentException.toString());
-		} catch (InvalidTypeException invalidTypeException) {
-			logger.error("any of the provided value is not of correct type to be bound to the corresponding bind variable ",
-					invalidTypeException);
-			throw new EieCacheCheckedException(
-					EieCacheErrorCodes.UNSUCCESSFULL_WRITE,
-					"any of the provided value is not of correct type to be bound to the corresponding bind variable",
-					invalidTypeException.toString());
-		} catch (NullPointerException nullPointerException) {
-			logger.error("null value found ",
-					nullPointerException);
-			throw new EieCacheCheckedException(
-					EieCacheErrorCodes.UNSUCCESSFULL_WRITE,
-					"null value found",
-					nullPointerException.toString());
-		}
-		catch (Exception exception) {
-			logger.error("error during writing data ", exception);
-			throw new EieCacheCheckedException(
-					EieCacheErrorCodes.UNSUCCESSFULL_WRITE,
-					"error during reading data", exception.toString());
-		}
-
+		writeFlag = ieieCacheDao.addEIEExternalReponse(eieRes);
 		return writeFlag;
 	}
 
